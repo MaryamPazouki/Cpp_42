a1427991a5cd915b87e2c3a3a88d86dcfc6d0949
MaryamPazouki <mpazouki@student.42wolfsburg.de>
2025-06-16 07:49:54 +0000

    send changes c00 ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -26,6 +26,7 @@ void make_upper(char **str){
 	}
 };
 
+
 // int main(int argc, char** argv)
 // {
 // 	int i;
@@ -47,21 +48,3 @@ void make_upper(char **str){
 // 	}	
 // 	return 0;
 // }
-
-
-
-int main(int argc, char** argv) {
-    if (argc < 2) {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-        return 0;
-    }
-
-    for (int i = 1; i < argc; ++i) {
-        std::string word = argv[i];
-        for (char& c : word)
-            std::cout << (char)std::toupper(c);
-    }
-
-    std::cout << std::endl;
-    return 0;
-}
